aab38fad2c87f0ee02ddf95c61f9d40ed78338f6
merveutas <72063524+merveutas@users.noreply.github.com>
2022-11-21 14:16:41 +0300

    Add project files.

---
+++ Blog.Core/Entities/EntityBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public abstract class EntityBase : IEntityBase
+    {
+    
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        public virtual Guid Id { get; set; } 
+        public virtual string CreateBy { get; set; }
+        public virtual string ModifiedBy { get; set; }
+        public virtual string DeletedBy { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime? ModifiedDate { get; set; }
+        public virtual DateTime? DeletedDate { get; set; }
+        public virtual bool IsDeleted { get; set; } = false;    
+    }
+}
